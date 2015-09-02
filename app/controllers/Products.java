5ac4d1afd3b738a4143a4992a3e31ac358f26f37
Ben Wu <wubin28@gmail.com>
2015-09-02 09:54:01 +0800

    Updated action method Products.newProduct() with product form.

--- app/controllers/Products.java
+++ app/controllers/Products.java
@@ -1,8 +1,10 @@
 package controllers;
 
 import models.Product;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.products.details;
 import views.html.products.list;
 
 import java.util.List;
@@ -11,13 +13,16 @@ import java.util.List;
  * Created by twer on 9/1/15.
  */
 public class Products extends Controller {
+
+    private static final Form<Product> productForm = Form.form(Product.class);
+
     public Result list() {
         List<Product> products = Product.findAll();
         return ok(list.render(products));
     }
 
     public Result newProduct() {
-        return TODO;
+        return ok(details.render(productForm));
     }
 
     public Result details(String ean) {
